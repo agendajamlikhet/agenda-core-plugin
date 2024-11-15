b3e329af8d796cca47939480769b18db8193a1ad
Farhan Maba <muhammadfarhanulhassan@gmail.com>
2024-11-15 21:53:36 +0100

    Update event-taxonomies.php

--- includes/event/event-taxonomies.php
+++ includes/event/event-taxonomies.php
@@ -29,6 +29,6 @@ function create_event_city_taxonomy()
         'show_tagcloud' => true,
     );
 
-    register_taxonomy('event_city', array('events'), $args);
+    register_taxonomy('event_city', array('event'), $args);
 }
 add_action('init', 'create_event_city_taxonomy');
